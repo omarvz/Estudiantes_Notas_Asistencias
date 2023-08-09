64a2dcfe6bcf8f38381bd713a11f94f220a1350c
omarvz <a20196261@pucp.edu.pe>
2023-08-08 20:29:18 -0500

    proyecto spring boot

---
+++ backend/gestion-academica/src/main/java/com/appress/gestionacademica/GestionAcademicaApplication.java
@@ -0,0 +1,13 @@
+package com.appress.gestionacademica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAcademicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionAcademicaApplication.class, args);
+	}
+
+}
